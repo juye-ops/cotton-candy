819ffc4567ed119ba8a7d0732c53353c22b60744
juye-ops <kjye.ops@gmail.com>
2023-05-11 21:30:23 +0900

    Fix: Modify proxy's target IP for IDE(docker)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <iframe src="http://172.24.0.2:8080/" title="codeserver" style={{ width: "100%", height: "100vh" }}>
+      <iframe src="http://172.24.0.4:8080/" title="codeserver" style={{ width: "100%", height: "100vh" }}>
       </iframe>
     </div>
   );
