d08a8010dbc9fe815579c5a68d61f8da6b765d9c
juye-ops <kjye.ops@gmail.com>
2023-05-11 17:39:40 +0900

    Feat: Init database for OS

--- backend/utils/database.py
+++ backend/utils/database.py
@@ -1,6 +1,13 @@
 def preprocess(client):
     db = client.docker
-    collection = db.platforms
+    os = [
+        {
+            "_id": 0,
+            "name": "ubuntu",
+            "version": ["3.7", "3.8", "3.9", "3.10"]
+        }
+    ]
+
     platform = [
         {
             "_id": 0,
@@ -14,4 +21,5 @@ def preprocess(client):
         }
     ]
 
-    collection.insert_many(platform)
+    db.platform.insert_many(platform)
+    db.os.insert_many(os)
