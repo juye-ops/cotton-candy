63715746162b816f66e0f9bc87f722c29020b8b7
juye-ops <kjye.ops@gmail.com>
2023-08-02 18:06:11 +0900

    Feat: Add func for network of each project

--- backend/utils/dind.py
+++ backend/utils/dind.py
@@ -18,3 +18,20 @@ def run(project_name, project_ports, project_envs):
         environment=project_envs, 
         detach=True
     )
+
+def create_network(name, subnet, gateway):
+    import docker
+    ipam_pool = docker.types.IPAMPool(
+        subnet = subnet,
+        gateway = gateway
+    )
+
+    ipam_config = docker.types.IPAMConfig(
+        pool_configs=[ipam_pool]
+    )
+
+    docker_cli.networks.create(
+        name=name,
+        driver="bridge",
+        ipam=ipam_config
+    )
