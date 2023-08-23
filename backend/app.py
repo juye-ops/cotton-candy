5ea4693e9b296b5cffe20387c7acd2e02b3669f4
juye-ops <kjye.ops@gmail.com>
2023-08-23 18:14:10 +0900

    🚑 fix: forward the proxy when on the remote

--- backend/app.py
+++ backend/app.py
@@ -7,24 +7,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.router.redirect_slashes = False
 
-origins = [
-    "http://localhost:3000",
-    # "*",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# app = FastAPI()
 app.include_router(container.router)
 app.include_router(project.router)
 app.include_router(version.router)
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=5000, access_log=False)
+    uvicorn.run("app:app", host="0.0.0.0", port=5000)
