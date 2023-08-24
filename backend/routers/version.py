64b9dcf3581a6e8831e48b60daa61396818ea37f
juye-ops <kjye.ops@gmail.com>
2023-08-24 16:58:17 +0900

    🐛 fix: rollback router ends with slash

--- backend/routers/version.py
+++ backend/routers/version.py
@@ -7,26 +7,26 @@ router = APIRouter(
 )
 
 
-@router.get("/framework/{app}/")
+@router.get("/framework/{app}")
 def _framework_version(app):
     data = FrameworkDB.get_version(app)
     ret = [x["version"] for x in data]
     return ret
 
 
-@router.get("/framework/")
+@router.get("/framework")
 def _framework_list():
     return FrameworkDB.get_list()
 
 
-@router.get("/os/{app}/")
+@router.get("/os/{app}")
 def _os_version(app):
     data = OSDB.get_version(app)
     ret = [x["version"] for x in data]
     return ret
 
 
-@router.get("/os/")
+@router.get("/os")
 def _os_list():
     data = OSDB.get_list()
     ret = [x["name"] for x in data]
